feba17ba45da21d14ffc3e97cade12ba022cdb82
peter-heywood <peter.heywood@homeoffice.gov.uk>
2026-06-11 16:17:29 +0100

    Added records

--- MyApp/hello_world/app.py
+++ MyApp/hello_world/app.py
@@ -1,10 +1,25 @@
 import json
 
-def lambda_handler(event, context):
+Records = [{"id": 0, "name":"Peter"},{"id": 1, "name":"John"},{"id": 2, "name":"Chris"}]
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "Welcome. Your API is up and running",
-        }),
-    }
+def lambda_handler(event, context):
+    if event['queryStringParameters']:
+        try:
+            return {
+            "statusCode": 200,
+            "body": json.dumps(Records[int(event['queryStringParameters']['id'])]),
+            }   
+        except:
+            return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": f"ID {event['queryStringParameters']['id']} not found",
+            }),
+            }   
+    else: 
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "No ID provided in URL",
+            }),
+        }
--- variables.tf
+++ variables.tf
@@ -4,5 +4,7 @@ locals {
   iam_policy_name = "lambda-iam-policy"
   iam_policy_description = "For deployment of Lambda SAM"
   iam_assume_role_name = "github-lambda-assume-role"
+
+  # You will need to change the below to the location of your GitHub repository
   repo = "peter-heywood/lambda-api-endpoint"
 }
